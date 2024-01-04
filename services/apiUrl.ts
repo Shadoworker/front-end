90651539253bb28e5b0681e5f31c4ed52907a6d1
habibe <habibe@kayfo.games>
2024-01-04 20:41:56 +0000

    https remote

--- services/apiUrl.ts
+++ services/apiUrl.ts
@@ -1,5 +1,5 @@
 
-export const baseUrl = "http://strapi-test-1-staging.eu-central-1.elasticbeanstalk.com";
+export const baseUrl = "https://strapi-test-1-staging.eu-central-1.elasticbeanstalk.com";
 // export const baseUrl = "http://localhost:1337";
 const apiUrl = baseUrl + "/api";
 
